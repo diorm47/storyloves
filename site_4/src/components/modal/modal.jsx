a269edc3b9398f28c23bfa6fb4107b8d286e80bb
dior <dior47m@gmail.com>
2023-02-16 15:09:32 +0500

    fixed bug with geo

--- site_4/src/components/modal/modal.jsx
+++ site_4/src/components/modal/modal.jsx
@@ -34,7 +34,7 @@ class Autocomplete extends Component {
     };
 
     let reqOptions = {
-      url: "http://ipwho.is/",
+      url: "https://ipwho.is/",
       method: "GET",
       headers: headersList,
     };
